5e527e9b4e469b95bb1e18531b4f0a9f95a8b44e
Daniel Ekman <knegge@gmail.com>
2022-11-25 12:15:20 +0100

    Add KISS format reader
    Signed-off-by: Daniel Ekman <knegge@gmail.com>

--- process_frames.py
+++ process_frames.py
@@ -4,6 +4,9 @@
 import argparse
 import io
 import os
+import struct
+from binascii import hexlify
+from datetime import datetime, timedelta
 
 
 def main():
@@ -11,16 +14,46 @@ def main():
         prog='process_frames <infile>',
         description='Process images from geoscan frames',
         epilog='')
-    parser.add_argument('infile', help="Input file, hex dump of frames")
+    parser.add_argument('infile', help="Input file with frames (default in hex)")
     parser.add_argument('-o', '--outfile', help="Output file prefix")
-    # parser.add_argument('-k', '--kiss', action='store_true', help="Input file format: KISS")
-    parser.add_argument('-v', '--verbose', action='store_true', help="Verbose output")
+    parser.add_argument('-k', '--kiss', action='store_true', help="Input file format: KISS")
+    # parser.add_argument('-p', '--pad', help="Byte to pad missing data")
+    parser.add_argument('-v', '--verbosity', action='count', default=0, help="Increase verbosity")
     args = parser.parse_args()
     if not args.outfile:
         args.outfile = os.path.splitext(args.infile)[0]
 
-    file = open(args.infile, 'r')
-    data = file.read().splitlines()
+    data = []
+    if args.kiss:
+        frame_tuples = []
+        try:
+            file = open(args.infile, 'rb')
+            frame_tuples = list(read_kiss_frames(file))
+        except AssertionError:
+            print('ERROR: KISS file inconsistent')
+            exit(1)
+        for (timestamp, frame) in frame_tuples:
+            if len(frame) == 64:
+                data.append(hexlify(frame).decode('ascii').upper())
+            elif args.verbosity >= 2:
+                print("Skipped frame with length: {}".format(len(frame)))
+
+    else:
+        filedata = []
+        try:
+            file = open(args.infile, 'r')
+            filedata = file.read().splitlines()
+        except UnicodeDecodeError:
+            print("ERROR: binary file ? try with --kiss")
+            exit(1)
+        for row in filedata:
+            row = row.replace(' ', '')
+            if '|' in row:
+                row = row.split('|')[-1]
+            if len(row) == 128:
+                data.append(row)
+            elif args.verbosity >= 2:
+                print("Skipped row with length: {}".format(len(row)))
 
     dataframes = ['01003E01', '01003E05', '01002605']
     headerlength = 16
@@ -28,16 +61,12 @@ def main():
     fileindex = 0
     chunks = 1
     for row in data:
-        row = row.replace(' ', '')
-        if '|' in row:
-            row = row.split('|')[-1]
-        if len(row) != 128:
-            continue
         header = row[:headerlength]
         cmd = row[:headerlength//2]
         addr = int((row[12:14] + row[10:12]), 16) % 32768
         payload = row[headerlength:]
-        # print("Frame: {} - {}".format(header, payload))
+        if args.verbosity >= 3:
+            print("Frame: {} - {}".format(header, payload))
         if cmd in dataframes:
             if outfile.closed:
                 extension = 'dat'
@@ -47,8 +76,10 @@ def main():
                 print("Writing to: {}".format(outfile.name))
                 fileindex += 1
             pos = addr - outfile.tell()
-            if pos != 0 and args.verbose:
+            if pos != 0 and args.verbosity >= 1:
                 print("Skipped {} bytes at pos {}. ".format(pos, addr - pos))
+                # if args.pad:
+                #     outfile.write(args.pad * pos)
                 # pos //= 2
                 # outfile.write(b'\xFF\xFF' * pos)
             outfile.seek(addr)
@@ -56,11 +87,63 @@ def main():
             chunks += 1
             if cmd == dataframes[2]:
                 outfile.close()
-                print("{} bytes written.".format(chunks * 56))
+                if args.verbosity >= 1:
+                    print("{} bytes written.".format(chunks * 56))
                 chunks = 1
         else:
-            if args.verbose:
+            if args.verbosity >= 2:
                 print("Skipped frame: {} - {}".format(header, payload))
+
+
+# kiss functions:
+# Copyright 2020 Fabian Schmidt <kerel+satnogs at mailbox.org>
+# Extended by SA2KNG <knegge at gmail.com>
+# SPDX-License-Identifier: GPL-3.0-or-later
+
+# Define KISS Special Codes
+# http://www.ax25.net/kiss.aspx
+FEND = b'\xc0'
+FESC = b'\xdb'
+TFEND = b'\xdc'
+TFESC = b'\xdd'
+
+
+def kiss_unescape(escaped_frame):
+    return escaped_frame.replace(
+        FESC + TFEND,
+        FEND
+    ).replace(
+        FESC + TFESC,
+        FESC
+    )
+
+
+def read_kiss_frames(kf):
+    parts = kf.read().split(FEND)
+
+    # Check that content starts with FEND byte (First Frame start)
+    assert (parts[0] == b'')
+
+    # Check that content is composed of (empty, timestamp, empty, frame_content)-frame tuples
+    parts_len = len(parts)
+    assert ((parts_len - 1) % 4 == 0)
+
+    frames_len = (parts_len - 1) // 4
+
+    for i in range(0, frames_len):
+        timestamp_raw = parts[i * 4 + 1]
+        escaped_frame = parts[i * 4 + 3]
+
+        # Make sure this is a KISS control frame (type = b'\x09')
+        assert (timestamp_raw[0] == 9)
+        # Make sure this is a data frame (type = b'\x00')
+        assert (escaped_frame[0] == 0)
+
+        timestamp_int, = struct.unpack('>Q', kiss_unescape(timestamp_raw[1:]))
+        epoch = datetime(1970, 1, 1)
+        timestamp_e = epoch + timedelta(seconds=timestamp_int // 1000)
+
+        yield timestamp_e, kiss_unescape(escaped_frame[1:])
 
 
 if __name__ == '__main__':
