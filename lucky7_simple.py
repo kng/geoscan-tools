7a2dfebb9d5ae80375966aceea08a872b17edc82
Daniel Ekman <knegge@gmail.com>
2023-01-15 20:43:54 +0100

    Add Lucky-7 image processing

--- lucky7_simple.py
+++ lucky7_simple.py
@@ -9,15 +9,17 @@ from sys import argv
 def main():
     if len(argv) != 2:
         print(f'Useage: {path.basename(argv[0])} <infile>\n'
-              'Process a single Geoscan-Edelveis image from hex/KISS frames.\n'
-              'Output will have the same name as the input, but with .jpg extension\n')
+              'Process Lucky-7 images from hex/KISS frames.\n'
+              'Output will have the same name as the input, but with ID and .jpg extension\n')
         exit(0)
     if not path.exists(argv[1]):
         print(f'File not found: {argv[1]}')
         exit(1)
     frames = parse_file(argv[1])
-    data = parse_frames(frames)
-    write_image(path.splitext(argv[1])[0] + '.jpg', data)
+    for pid in set(get_packet_ids(frames)):
+        print(pid)
+        data = parse_frames(pid, frames)
+        write_image(f'{path.splitext(argv[1])[0]}_{pid}.jpg', data)
 
 
 def parse_file(infile):
@@ -35,7 +37,7 @@ def parse_hexfile(f):
         row = row.replace(' ', '').strip()
         if '|' in row:
             row = row.split('|')[-1]
-        if len(row) == 128:
+        if len(row) >= 70:
             data.append(row)
     return data
 
@@ -49,20 +51,30 @@ def parse_kissfile(infile):
     return data
 
 
-def parse_frames(data):
+def get_packet_ids(data):
+    pids = []
+    offset = 49152  # 0xC000
+    for row in data:
+        oid = int(row[0:2], 16)
+        obc = int(row[2:6], 16)
+        packets = int(row[10:14], 16)
+        if (oid == 128 or oid == 0) and obc >= offset:
+            pids.append(packets)
+    return pids
+
+
+def parse_frames(pid, data):
     image = BytesIO()
-    offset = 16384  # old 32768
+    offset = 49152  # 0xC000
     for row in data:
-        if row[0:8].upper() == '01003E01':
-            offset = int((row[12:14] + row[10:12]), 16)
-            break
-    for row in data:
-        cmd = row[0:4]
-        addr = int((row[12:14] + row[10:12]), 16) - offset
-        dlen = (int(row[4:6], 16) + 2) * 2
-        payload = row[16:dlen]
-        if cmd == '0100' and addr >= 0:
-            image.seek(addr)
+        oid = int(row[0:2], 16)
+        obc = int(row[2:6], 16)
+        # mcu = int(row[6:10], 16)
+        packets = int(row[10:14], 16)
+        payload = row[14:70]
+        # print(f'ID {oid:4}, OBC {obc:5}, MCU {mcu:5}, packets {packets:5}: {row[0:14]} {payload}')
+        if (oid == 128 or oid == 0) and obc >= offset and packets == pid:
+            image.seek((obc - offset) * 28)
             image.write(bytes.fromhex(payload))
     return image
 
